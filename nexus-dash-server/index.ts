40cc3998d8bddaad1fcd1d3ff0a8500d67ae413c
IamPial <pialname394@gmail.com>
2026-07-14 18:07:26 +0600

    config the index.ts file with dotenv mongodb

---
+++ nexus-dash-server/index.ts
@@ -0,0 +1,57 @@
+import 'dotenv/config';
+import express, { type Express, type Request, type Response } from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+
+
+
+const uri: string  = process.env.MONGODB_URI as string;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("nexus-dash-db");
+
+    const exploreCollection = db.collection("explore");
+
+
+
+
+    //Explore related API
+    app.get('/explore', async (req: Request, res: Response) => {
+        const result = await exploreCollection.find().toArray();
+        res.json(result); 
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch();
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
