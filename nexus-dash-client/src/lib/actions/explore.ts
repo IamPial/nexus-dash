44f450fb89847ea19f5a568565b363e2da5b38e7
IamPial <pialname394@gmail.com>
2026-07-15 09:05:15 +0600

    create explore data func

---
+++ nexus-dash-client/src/lib/actions/explore.ts
@@ -0,0 +1,26 @@
+'use server'
+
+const baseURl = process.env.NEXT_PUBLIC_API_URL
+
+interface ExploreItems {
+  title: string;
+  description: string
+  price: string,
+  date:string,
+  image?: string;
+  location: string,
+  category: string
+}
+
+export const createExploreItems = async(exploreData:ExploreItems)=>{
+    const res = await fetch(`${baseURl}/api/explore`,{
+        method:"POST",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify(exploreData)
+    })
+
+    const data = await res.json()
+    return data
+}
